a6d34a80b87571fe6d6835bf91ee19d09d1c14d5
Rayyane Houmine <houmrayyane@gmail.com>
2025-01-11 15:29:49 -0500

    documenting js

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ let listWords = [
     "soccer", "basketball", "football", "volleyball", "tennis", "baseball", "swimming", "running", "cycling", "boxing",
     "python", "javascript", "html", "css", "java", "swift", "c++", "ruby", "php", "rust", "apple", "banana", "cherry", "dog", 
     "elephant", "fish", "grape", "hat", "ice", "jacket", "kite", "lemon", "mountain", "night", "orange", "parrot", "queen", "rose",
-     "snake", "tree", "umbrella", "vulture", "water", "xylophone", "yellow", "zebra", "airplane", "book", "cat", "dragon", "egg", 
-     "frog", "guitar", "house", "ink", "joker", "king", "lighthouse", "mouse", "necklace", "octopus", "piano", "quilt", "rocket", 
-     "sun", "train", "unicorn", "vase", "whale", "x-ray", "yarn", "zoo", "actor", "ball", "clock", "door", "engine", "fire", "grapevine", 
-     "hammer", "icecream", "juice", "koala", "lamp", "magnet", "notebook", "octagon", "pizza", "quicksand", "rosebush", "snow", "telescope",
-      "umbrella", "vaccine", "window", "yoga", "ant", "butterfly", "car", "dolphin", "elephant", "flute", "garden", "hummingbird", "illusion", 
-      "jellyfish", "keyboard", "lizard", "mirror", "noodle", "octopus", "penguin", "quiver", "raccoon", "scooter", "tiger", "universe", "vampire", 
-      "waterfall", "xenon", "yacht", "zipper", "asparagus", "balloon", "castle", "diamond", "eggplant", "fox", "goose", "hamster", "island", "jaguar",
-       "key", "lemonade", "monkey", "needle", "owl", "puzzle", "quiz", "robot", "seagull", "tooth", "utensil", "vegetable", "wheel", "xylophonist", "yell",
-        "zigzag", "avocado", "bicycle", "cactus", "dandelion", "elephant", "feather", "grapefruit", "highway", "iceberg", "jaguar", "kingfisher", "leprechaun", 
-        "meteor", "neutron", "oak", "pirate", "queenbee", "rocketship", "sunflower", "tornado", "underwater", "volcano", "whistle", "xmas", "yawning", "zeppelin", 
-        "algae", "butter", "caterpillar", "duck", "earth", "fruit", "gator", "horizon", "inkling", "jewel", "kangaroo", "lipstick", "moose", "napkin", "obstacle", 
-        "pineapple", "quickstep", "rabbit", "silk", "tissue", "universe", "vine", "wildlife", "xerox", "yeti", "zinc", "antelope", "blossom", "clover", "daisy", "elephant", 
-        "fiddle", "grapevines", "helium", "idea", "jalapeno", "kiwi", "lamp", "magnolia", "nightingale", "octopus", "puzzle", "quaint", "riddle", "sundial", "thunder", "unicorn", 
-        "viper", "windmill", "xander", "yak", "zebra"];
+    "snake", "tree", "umbrella", "vulture", "water", "xylophone", "yellow", "zebra", "airplane", "book", "cat", "dragon", "egg", 
+    "frog", "guitar", "house", "ink", "joker", "king", "lighthouse", "mouse", "necklace", "octopus", "piano", "quilt", "rocket", 
+    "sun", "train", "unicorn", "vase", "whale", "x-ray", "yarn", "zoo", "actor", "ball", "clock", "door", "engine", "fire", "grapevine", 
+    "hammer", "icecream", "juice", "koala", "lamp", "magnet", "notebook", "octagon", "pizza", "quicksand", "rosebush", "snow", "telescope",
+    "umbrella", "vaccine", "window", "yoga", "ant", "butterfly", "car", "dolphin", "elephant", "flute", "garden", "hummingbird", "illusion", 
+    "jellyfish", "keyboard", "lizard", "mirror", "noodle", "octopus", "penguin", "quiver", "raccoon", "scooter", "tiger", "universe", "vampire", 
+    "waterfall", "xenon", "yacht", "zipper", "asparagus", "balloon", "castle", "diamond", "eggplant", "fox", "goose", "hamster", "island", "jaguar",
+    "key", "lemonade", "monkey", "needle", "owl", "puzzle", "quiz", "robot", "seagull", "tooth", "utensil", "vegetable", "wheel", "xylophonist", "yell",
+    "zigzag", "avocado", "bicycle", "cactus", "dandelion", "elephant", "feather", "grapefruit", "highway", "iceberg", "jaguar", "kingfisher", "leprechaun", 
+    "meteor", "neutron", "oak", "pirate", "queenbee", "rocketship", "sunflower", "tornado", "underwater", "volcano", "whistle", "xmas", "yawning", "zeppelin", 
+    "algae", "butter", "caterpillar", "duck", "earth", "fruit", "gator", "horizon", "inkling", "jewel", "kangaroo", "lipstick", "moose", "napkin", "obstacle", 
+    "pineapple", "quickstep", "rabbit", "silk", "tissue", "universe", "vine", "wildlife", "xerox", "yeti", "zinc", "antelope", "blossom", "clover", "daisy", "elephant", 
+    "fiddle", "grapevines", "helium", "idea", "jalapeno", "kiwi", "lamp", "magnolia", "nightingale", "octopus", "puzzle", "quaint", "riddle", "sundial", "thunder", "unicorn", 
+    "viper", "windmill", "xander", "yak", "zebra"];
 
-
+//Declaring the useful variables 
 let currentWord = "";
 let score = 0;
 let lives = 10;
@@ -32,18 +32,23 @@ let Start = false;
 let movingWords = [];
 let spawnInterval = null ;
 
+//Useful selectors 
 const wordsContainer = document.getElementById("words"); 
 const scoreSpan = document.getElementById("score");
 const livesSpan = document.getElementById("lives");
 const typedInput = document.getElementById("typed-input");
 const startButton = document.getElementById("start-button");
 
+//This function chooses one word from the list of words declared above 
 function getRandomWord() {
     let randomIndex = Math.floor(Math.random() * listWords.length);
     return listWords[randomIndex];
 }
 
+//This function makes the word chosen by the function getRandomWord fall vertically
+//It also decreases the lives by 1 if the player can't type the word correctly before it disappears from the window 
 function moveWord(){
+
     let randomWord = getRandomWord();
 
     let wordElement = document.createElement("div");
@@ -90,6 +95,7 @@ typedInput.addEventListener("keydown", function (event) {
       }
 });
 
+//This function verifies if the word typed by the player matches the spelling of any falling word
 function checkTypedWord() {
     let typedValue = typedInput.value.trim();
     if (!typedValue) return;
@@ -133,6 +139,7 @@ function loseLife() {
     }
 }
 
+//This function displays the game over window and the final score
 function GameOver(){
   const finalScoreSpan = document.getElementById("final-score");
   finalScoreSpan.innerText = score; 
@@ -147,6 +154,7 @@ document.getElementById("game-over-window").style.display = "none";
   startGame();
 });
 
+//This function resets the variables socre , lives to initial values and removes the active words from the screen 
 function resetGame() {
     Start = false;
     clearInterval(spawnInterval);
@@ -165,6 +173,7 @@ function resetGame() {
     typedInput.value = "";
 }
 
+//This function starts the game
 function startGame() {
     if (!Start) {
       Start = true;
